d1611f633d68819962d63de2cc0de43829b7eb83
Matthew Robinson <matt_robinson1@yahoo.com>
2019-10-11 17:05:58 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -23,17 +23,17 @@ class Student
     end
   end
 
-  def self.count_all_students_in_grade_9
-    sql = <<-SQL
-      SELECT COUNT (*)
-      FROM students
-      WHERE grade=9
-    SQL
-
-    DB[:conn].execute(sql).map do |row|
-      self.new_from_db(row)
-    end
+ def self.count_all_students_in_grade_9
+  sql = <<-SQL
+  SELECT *
+  FROM students
+  WHERE students.grade = 9
+  SQL
+  DB[:conn].execute(sql).collect do |row|
+    self.new_from_db(row)
   end
+end
+
 
   def self.students_below_12th_grade
     sql = <<-SQL
